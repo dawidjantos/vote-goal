5a831e1d5ba4f010814f5c696868edf3d9f32346
Dawid Jantos <d.jantos@outlook.com>
2024-03-12 18:54:12 +0100

    middleware fix v5

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,28 @@ import type {NextRequest} from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  console.log(`referer: ${request.headers.get('referer')}`);
-  if (request.nextUrl.pathname.startsWith('/admin')) {
-    return NextResponse.rewrite(new URL('/', request.url))
-  } else if (request.headers.get('referer') === 'http://tprzybylski.pl/') {
-    return NextResponse.rewrite(new URL('/', request.url));
+
+  if (request.cookies.has('referer')) {
+    return NextResponse
   } else {
-    return NextResponse.redirect('http://tprzybylski.pl');
+    console.log(`referer: ${request.headers.get('referer')}`);
+
+    if (request.nextUrl.pathname.startsWith('/admin')) {
+      return NextResponse.rewrite(new URL('/', request.url))
+    } else if (request.headers.get('referer') === 'http://tprzybylski.pl/') {
+      let now = new Date();
+      now.setTime(now.getTime() + 1 * 3600 * 1000);
+      const response = NextResponse.next()
+      response.cookies.set({
+        name: 'referer',
+        value: 'referer',
+        path: '/',
+        expires: now,
+      })
+      return response;
+    } else {
+      return NextResponse.redirect('http://tprzybylski.pl');
+    }
   }
 }
 
